237d591e49a9dd8680e4667217c5ed4c45190e9a
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2022-12-30 13:59:41 +0530

    create gui of which system..

--- CHECK.py
+++ CHECK.py
@@ -3,4 +3,6 @@ root=Tk()
 root.title('Convertor')
 root.geometry('1280x720')
 
-Label(root,text='')
+Label(root,text='Conversion System',font=('times new rommon ',50,'bold'),bg='gray',fg='white',relief=RIDGE).pack(pady=10)
+
+root.mainloop()
