4dd71fff4885f0bdd4e24c99399b01bd4c6d508b
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2022-12-30 14:25:48 +0530

    change  font  & colour ..

--- CHECK.py
+++ CHECK.py
@@ -3,24 +3,26 @@ root=Tk()
 root.title('Convertor')
 root.geometry('1280x720')
 
-Label(root,text='Conversion System',font=('times new rommon ',50,'bold'),bg='gray',fg='white',relief=RIDGE).pack(pady=10)
+Label(root,text='Conversion System',font=('Poppins',50,'bold'),bg='gray',fg='white',relief=RIDGE).pack(pady=10)
 
 
-n=Label(root,text='Enter Number :',font=('times new rommon ',20,'bold'),fg='blue')
+n=Label(root,text='Enter Number :',font=('Poppins',20,'bold'),fg='blue')
 n.place(x=300,y=150)
 
-b=Label(root,text='Binary :',font=('times new rommon ',20,'bold'),fg='blue')
+b=Label(root,text='Binary :',font=('Poppins',20,'bold'),fg='green')
 b.place(x=300,y=230)
 
-d=Label(root,text='Decimal :',font=('times new rommon ',20,'bold'),fg='blue')
+d=Label(root,text='Decimal :',font=('Poppins',20,'bold'),fg='orange')
 d.place(x=300,y=310)
 
-h=Label(root,text='HexaDecimal :',font=('times new rommon ',20,'bold'),fg='blue')
+h=Label(root,text='HexaDecimal :',font=('Poppins',20,'bold'),fg='deeppink')
 h.place(x=300,y=390)
 
-o=Label(root,text='Octal :',font=('Poppins''sans-serif',20,'bold'),fg='blue')
+o=Label(root,text='Octal :',font=('Poppins ',20,'bold'),fg='purple')
 o.place(x=300,y=470)
 
+e1= Entry(root,font='arial 20 ',fg='red',justify=CENTER,relief=GROOVE)
+e1.place(x=650,y=150)
 
 
 
