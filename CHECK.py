d383fc974dfbb517aa3c6d7f5b4b45ac368cbb47
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2022-12-30 14:16:23 +0530

    create labels of number system..

--- CHECK.py
+++ CHECK.py
@@ -5,4 +5,24 @@ root.geometry('1280x720')
 
 Label(root,text='Conversion System',font=('times new rommon ',50,'bold'),bg='gray',fg='white',relief=RIDGE).pack(pady=10)
 
+
+n=Label(root,text='Enter Number :',font=('times new rommon ',20,'bold'),fg='blue')
+n.place(x=300,y=150)
+
+n=Label(root,text='Binary :',font=('times new rommon ',20,'bold'),fg='blue')
+n.place(x=300,y=230)
+
+n=Label(root,text='Decimal :',font=('times new rommon ',20,'bold'),fg='blue')
+n.place(x=300,y=310)
+
+n=Label(root,text='HexaDecimal :',font=('times new rommon ',20,'bold'),fg='blue')
+n.place(x=300,y=390)
+
+n=Label(root,text='Octal :',font=('Poppins''sans-serif',20,'bold'),fg='blue')
+n.place(x=300,y=470)
+
+
+
+
+
 root.mainloop()
